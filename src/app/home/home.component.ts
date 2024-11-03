985e43325906119d4d9e3369ff53bbd2fbe4bc61
rifqitaufiqurrohman1@gmail.com <rifqitaufiqurrohman1@gmail.com>
2024-11-03 15:32:04 +0700

    refactor: adjust currentdate & fitur search articels

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,34 +1,74 @@
 import { Component, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 import { NytimesService } from '../services/nytimes.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import * as THREE from 'three';
+import { DatePipe } from '@angular/common';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
+  providers: [DatePipe],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit, AfterViewInit {
+  currentDate: string | null;
   categories: string[] = ['home', 'world', 'business', 'arts', 'opinion'];
   selectedCategory: string = 'home';
   isLoading: boolean = false;
+
+  private searchQuery$ = new BehaviorSubject<string>('');
+
+  topMostArticles$: Observable<any[]> | null;
+  categoryArticles$: Observable<any[]> | null;
+  filteredCategoryArticles$: Observable<any[]>;
+  filteredTopMostArticles$: Observable<any[]>;
 
   // Initial visible items
   visibleCategoryArticles: number = 4;
   visibleTopMostArticles: number = 1;
 
-  // Observables
-  topMostArticles$;
-  categoryArticles$;
-
   constructor(
     private readonly nytimesService: NytimesService,
-    private elRef: ElementRef
+    private elRef: ElementRef,
+    private datePipe: DatePipe
   ) {
+    this.currentDate = this.datePipe.transform(new Date(), 'EEEE, MMMM d, y');
+
+    // Mendapatkan data artikel dari service
     this.topMostArticles$ = this.nytimesService.topMostArticles$;
     this.categoryArticles$ = this.nytimesService.categoryArticles$;
+
+    // Inisialisasi observables untuk artikel yang sudah difilter
+    this.filteredCategoryArticles$ = combineLatest([
+      this.categoryArticles$,
+      this.searchQuery$,
+    ]).pipe(
+      map(([articles, query]) =>
+        articles.filter(
+          (article) =>
+            article.title.toLowerCase().includes(query.toLowerCase()) ||
+            article.abstract.toLowerCase().includes(query.toLowerCase())
+        )
+      )
+    );
+
+    this.filteredTopMostArticles$ = combineLatest([
+      this.topMostArticles$,
+      this.searchQuery$,
+    ]).pipe(
+      map(([articles, query]) =>
+        articles.filter(
+          (article) =>
+            article.title.toLowerCase().includes(query.toLowerCase()) ||
+            article.abstract.toLowerCase().includes(query.toLowerCase())
+        )
+      )
+    );
   }
 
   ngOnInit(): void {
@@ -42,17 +82,15 @@ export class HomeComponent implements OnInit, AfterViewInit {
     }
   }
 
-  fetchTopMostNews(): void {
-    this.nytimesService.getTopMostNews();
+  async fetchTopMostNews(): Promise<void> {
+    await this.nytimesService.getTopMostNews();
   }
 
-  fetchCategoryNews(category: string): void {
+  async fetchCategoryNews(category: string): Promise<void> {
     this.selectedCategory = category;
     this.isLoading = true;
-    this.nytimesService.getCategoryNews(category);
-    this.nytimesService.categoryArticles$.subscribe(() => {
-      this.isLoading = false;
-    });
+    await this.nytimesService.getCategoryNews(category);
+    this.isLoading = false;
   }
 
   loadMoreCategoryArticles(): void {
@@ -61,6 +99,11 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   loadMoreTopMostArticles(): void {
     this.visibleTopMostArticles += 5;
+  }
+
+  onSearch(query: string): void {
+    this.searchQuery$.next(query.toLowerCase());
+    console.log('seach', this.searchQuery$);
   }
 
   createScene(): void {
